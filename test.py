34fa788802835ea74b795a68790d5bdcae631539
chrisirakiza <dushichris@yahoo.fr>
2022-11-09 21:18:38 -0500

    comments

---
+++ test.py
@@ -0,0 +1,11 @@
+# Testing automatically features of the application 
+
+#Test will include: 
+# 1. Creating a new user in the admin profile ( client , manager and supplier)
+#2. Logging in with the correct password and without the correct password 
+#3. Verifying the types of access each user have 
+#4. Making an order less than 5000$ and verifying if an automatic quote is sent and the order is accepted 
+#5. Making an order of more than 5000$ 
+
+
+ 
