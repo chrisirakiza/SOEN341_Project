d55fa0c58bbdfe6b89b51690fe85e20f1b93c0df
roshansadath <69693888+roshansadath@users.noreply.github.com>
2022-11-10 15:22:07 -0500

    Add files via upload

--- Create_Database.py
+++ Create_Database.py
@@ -1,13 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-
-
 import mysql.connector
 from mysql.connector import Error
 
-class design_database:
+class Create_Database:
             
     host, user, pwd, dbName = '', '', '', ''
     def __init__(self, host, user, password, dbName):
@@ -60,27 +54,33 @@ class design_database:
 
     #DESIGNING QUERIES TO BUILD DATABASE
 
-    create_table_user =                """CREATE TABLE USER( 
+    create_table_user =                """CREATE TABLE USER(
+                                            id INTEGER NOT NULL AUTO_INCREMENT,
                                             name VARCHAR(100),
                                             userID VARCHAR(10) PRIMARY KEY NOT NULL,
                                             password VARCHAR(20),
-                                            userType VARCHAR(50))"""
+                                            userType VARCHAR(50),
+                                            INDEX(id))"""
    
-    create_table_procurement_request = """CREATE TABLE PROCUREMENT_REQUEST( 
+    create_table_procurement_request = """CREATE TABLE PROCUREMENT_REQUEST(
+                                            id INTEGER NOT NULL AUTO_INCREMENT,
                                             requestNumber VARCHAR(20) PRIMARY KEY NOT NULL,
                                             itemName VARCHAR(200),
+                                            quantity INTEGER,
                                             generatedBy VARCHAR(100) REFERENCES USER(name),
                                             assignedManager VARCHAR(100) REFERENCES USER(name),
-                                            status BOOLEAN DEFAULT(0),
-                                            acceptedQuoteID VARCHAR(10)
+                                            status INTEGER,
+                                            acceptedQuoteID VARCHAR(10),
+                                            INDEX(id)
                                             )"""
     
     create_table_quote =               """CREATE TABLE QUOTE(
+                                            id INTEGER NOT NULL AUTO_INCREMENT, 
                                             quoteID VARCHAR(10) PRIMARY KEY NOT NULL,
                                             requestID VARCHAR(20),
                                             price FLOAT(7, 2),
                                             supplierName VARCHAR(100) REFERENCES USER(name),
-
+                                            INDEX(id),
                                             FOREIGN KEY (requestID) REFERENCES PROCUREMENT_REQUEST(requestNumber)
                                             ON DELETE CASCADE)"""
 
@@ -91,14 +91,13 @@ class design_database:
                                             companyName VARCHAR(200),
                                             productType VARCHAR(100),
                                             contactInfo VARCHAR(200),
-
                                             FOREIGN KEY (supplierID) REFERENCES USER(userID)
                                             ON UPDATE CASCADE ON DELETE CASCADE
                                             )"""
     
 
 
-DB = design_database('localhost', 'root', '#Snroshan1998', "soen341")
+DB = Create_Database('localhost', 'root', '#Snroshan1998', "soen341")
 connection = DB.connect_to_database()
 
 DB.execute_query(connection, DB.create_table_user)
@@ -106,3 +105,27 @@ DB.execute_query(connection, DB.create_table_procurement_request)
 DB.execute_query(connection, DB.create_table_quote)
 DB.execute_query(connection, DB.create_table_company)
 
+
+# DB2 = Create_Database('localhost', 'root', '#Snroshan1998', "soen341")
+# connection = DB2.connect_to_database()
+
+# def add_user(name, user_id, password, user_type):
+#     query_add_user = """ INSERT INTO USER VALUES (default, '%s', '%s', '%s', '%s')"""%(name, user_id, password, user_type)    
+#     DB2.execute_query(connection, query_add_user)
+    
+# def get_user(user_id):
+#     query_get_user = """SELECT * FROM USER WHERE USER.userID = "%s" """ %(user_id)
+#     user_data = DB2.read_query(connection, query_get_user)
+#     return user_data[0][1], user_data[0][2], user_data[0][3], user_data[0][4]
+
+# def get_counter_value():
+#     query_get_last_count = """SELECT MAX(id) FROM USER"""
+#     counter = DB2.read_query(connection, query_get_last_count)
+#     return int(counter[0][0])
+
+#query_add_request = """INSERT INTO PROCUREMENT_REQUEST VALUES (default, '%s', '%s', %d, '%s', '%s', %d))""" %(rnum, itemName, quantity, generatedBy, assignedManager, status)
+#Create_Database.execute_query(connection, query_add_request)
+
+# add_user('Rosh', 'A0001', 'bleh', 'Admin')
+# print(get_user('A0000'))
+# get_counter_value()
