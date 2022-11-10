5a74fd325f593670a05c6662507901bc3571e8b4
zacharyyve <zacharyyve@gmail.com>
2022-11-10 14:17:00 -0500

    Updated Create_Database.py using notebook file

--- Create_Database.py
+++ Create_Database.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-
-
 import mysql.connector
 from mysql.connector import Error
 
@@ -60,27 +54,32 @@ class design_database:
 
     #DESIGNING QUERIES TO BUILD DATABASE
 
-    create_table_user =                """CREATE TABLE USER( 
+    create_table_user =                """CREATE TABLE USER(
+                                            id INTEGER NOT NULL AUTO_INCREMENT,
                                             name VARCHAR(100),
                                             userID VARCHAR(10) PRIMARY KEY NOT NULL,
                                             password VARCHAR(20),
-                                            userType VARCHAR(50))"""
+                                            userType VARCHAR(50),
+                                            INDEX(id))"""
    
-    create_table_procurement_request = """CREATE TABLE PROCUREMENT_REQUEST( 
+    create_table_procurement_request = """CREATE TABLE PROCUREMENT_REQUEST(
+                                            id INTEGER NOT NULL AUTO_INCREMENT,
                                             requestNumber VARCHAR(20) PRIMARY KEY NOT NULL,
                                             itemName VARCHAR(200),
                                             generatedBy VARCHAR(100) REFERENCES USER(name),
                                             assignedManager VARCHAR(100) REFERENCES USER(name),
                                             status BOOLEAN DEFAULT(0),
-                                            acceptedQuoteID VARCHAR(10)
+                                            acceptedQuoteID VARCHAR(10),
+                                            INDEX(id)
                                             )"""
     
     create_table_quote =               """CREATE TABLE QUOTE(
+                                            id INTEGER NOT NULL AUTO_INCREMENT, 
                                             quoteID VARCHAR(10) PRIMARY KEY NOT NULL,
                                             requestID VARCHAR(20),
                                             price FLOAT(7, 2),
                                             supplierName VARCHAR(100) REFERENCES USER(name),
-
+                                            INDEX(id),
                                             FOREIGN KEY (requestID) REFERENCES PROCUREMENT_REQUEST(requestNumber)
                                             ON DELETE CASCADE)"""
 
@@ -106,3 +105,24 @@ DB.execute_query(connection, DB.create_table_procurement_request)
 DB.execute_query(connection, DB.create_table_quote)
 DB.execute_query(connection, DB.create_table_company)
 
+
+# DB2 = design_database('localhost', 'root', '#Snroshan1998', "soen341")
+# connection = DB2.connect_to_database()
+
+# def add_user(name, user_id, password, user_type):
+#     query_add_user = """ INSERT INTO USER VALUES (default, '%s', '%s', '%s', '%s')"""%(name, user_id, password, user_type)    
+#     DB2.execute_query(connection, query_add_user)
+    
+# def get_user(user_id):
+#     query_get_user = """SELECT * FROM USER WHERE USER.userID = "%s" """ %(user_id)
+#     user_data = DB2.read_query(connection, query_get_user)
+#     return user_data[0][1], user_data[0][2], user_data[0][3], user_data[0][4]
+
+# def get_counter_value():
+#     query_get_last_count = """SELECT MAX(id) FROM USER"""
+#     counter = DB2.read_query(connection, query_get_last_count)
+#     return int(counter[0][0])
+
+# add_user('Rosh', 'A0001', 'bleh', 'Admin')
+# print(get_user('A0000'))
+# get_counter_value()
