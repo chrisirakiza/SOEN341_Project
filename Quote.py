b4eb30800877782c11e337a5e4ab1f24b3c3201b
chrisirakiza <dushichris@yahoo.fr>
2022-11-10 02:42:38 -0500

    created new quote class with getters and setters

---
+++ Quote.py
@@ -0,0 +1,37 @@
+
+# implementing the class that will handle the quote (i.e price of each item)
+
+import Users
+
+
+class Quote:
+
+# constructor 
+
+  def __init__(self,Price, supplier):
+
+    self.Price = Price 
+    self.user = supplier
+
+# getter
+def GetPrice(self,Price) -> float:
+        return self.Price
+
+# Setter 
+def SetPrice(self, p) -> str:
+        self.Price = p
+        
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
